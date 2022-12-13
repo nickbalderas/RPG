3f502b65121afa6f57cf11b1409e9d820fff1545
nickB <nbalderas524@icloud.com>
2022-12-13 05:30:16 -0800

    Created a fixed follow camera

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,13 +7,6 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
