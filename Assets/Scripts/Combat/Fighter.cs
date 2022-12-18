5f6ab8f8199511fed3c6e911e97a83df04c77259
nickB <nbalderas524@icloud.com>
2022-12-18 06:48:33 -0800

    Trigger animation in code

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -21,7 +21,13 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehavior();
             }
+        }
+
+        private void AttackBehavior()
+        {
+            GetComponent<Animator>().SetTrigger("attack");
         }
 
         private bool GetIsInRange()
@@ -38,6 +44,12 @@ namespace RPG.Combat
         public void Cancel()
         {
             _target = null;
+        }
+        
+        // Animation Event
+        void Hit()
+        {
+            
         }
     }
 }
